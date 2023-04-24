70dd1b58b96b6aee5319cbb37a503c5d76bec900
Hitesh Deore <113298042+hiteshdeore@users.noreply.github.com>
2023-04-24 23:59:24 +0530

    Add files via upload

---
+++ SringSOA Test/2.AccountList Lightning app/AccountList.app
@@ -0,0 +1,5 @@
+<aura:application  >
+
+    <c:AccountList />
+
+</aura:application>
---
+++ SringSOA Test/2.AccountList Lightning app/AccountList.js
@@ -0,0 +1,14 @@
+({
+    doInit : function(component, event, helper) {
+        var action = component.get("c.getRecentAccounts");
+        action.setCallback(this, function(response) {
+            var state = response.getState();
+            if (state === "SUCCESS") {
+                component.set("v.accounts", response.getReturnValue());
+            } else {
+                console.log("Failed with state: " + state);
+            }
+        });
+        $A.enqueueAction(action);
+    }
+})
